ab892164a1e5292cd10d38ae7f02f44143e99d39
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-05-27 12:49:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -25,7 +25,7 @@ def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional("Yordin", language)
+introduction_with_language_optional("Yordin")
 
 
 
